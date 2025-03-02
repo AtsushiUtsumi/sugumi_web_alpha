b481b0716fb010d3e80b6767005768621553b507
AtsushiUtsumi <debtgx103@gmail.com>
2025-03-02 12:05:59 +0900

    ファイル作成機能追加

--- app.py
+++ app.py
@@ -8,4 +8,12 @@ app.register_blueprint(health_blueprint)# 「health」を追加
 @app.route('/')
 @app.route('/home/<name>')
 def hello(name=None):
+    file = open('test.txt', 'a')
+    file.write("HOGE")
+    file.close()
+    file = open('test.txt', 'r')
+    lines = file.readlines()
+    file.close()
+    if len(lines) > 0:
+        return render_template('hello.html', name=lines)
     return render_template('hello.html', name=name)
