9bbe66186107959d0041ad8788a08eb2a1dbeb52
AtsushiUtsumi <debtgx103@gmail.com>
2025-05-31 16:20:02 +0900

    DBを使わないように変更

--- sugumi_web/settings.py
+++ sugumi_web/settings.py
@@ -56,12 +56,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sugumi_web.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
